d8dbaa2644ab60672e8fc4dee8384d66d4b0a406
unknown <Maajd Bashir>
2020-06-04 00:43:03 -0700

    odddeven

---
+++ demoodd_even.py
@@ -0,0 +1,11 @@
+print("Enter The First Number")
+x = int(input())
+print("Enter The Second Number")
+y = int(input())
+if x == y:
+  print("Numbers are equal")
+else:
+  if x < y :
+    print (" x is less than y")
+  else:
+    print("x is greater than Y")
